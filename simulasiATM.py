f48cbcf8990d83e47585294c2134558604db6ece
pasbraska <pasbraska.official@gmail.com>
2023-12-31 19:50:46 +0700

    update

--- simulasiATM.py
+++ simulasiATM.py
@@ -3,70 +3,62 @@ saldo = 100000
 def cek_saldo():
     print('')
     print('========================================')
-    print('saldo anda saat ini adalah: Rp',saldo)
+    print('saldo anda saat ini adalah: Rp', saldo)
     print('========================================')
-    
+
 def transfer():
     global saldo
-    norek_tujuan=int(input('masukkan nomer rekening tujuan: '))
-    if norek_tujuan <=10:
-    	print('norek tidak ada')
+    norek_tujuan = int(input('masukkan nomer rekening tujuan: '))
+    if norek_tujuan <= 10:
+        print('norek tidak ada')
     else:
-    	jumlah=float(input('transfer berapa?: Rp'))
-    print('')
-    
-    if jumlah > saldo:
-        print('=========================')
-        print('maaf saldo tidak cukup')
-        print('=========================')
-    else:
-        saldo-=jumlah
-        print('=========================')
-        print('transfer berhasil')
-        print('=========================')
-        cek_saldo()
+        jumlah = float(input('transfer berapa?: Rp'))
+        print('')
         
+        if jumlah > saldo:
+            print('=========================')
+            print('maaf saldo tidak cukup')
+            print('=========================')
+        else:
+            saldo -= jumlah
+            print('=========================')
+            print('transfer berhasil')
+            print('=========================')
+            cek_saldo()
+
 def tarik_tunai():
     global saldo
-    tarik=float(input('Tarik tunai berapa?: Rp'))
+    tarik = float(input('Tarik tunai berapa?: Rp'))
     if tarik > saldo:
         print('=========================')
         print('maaf saldo tidak cukup')
         print('=========================')
     else:
+        saldo -= tarik
         print('=========================')
         print('tarik tunai berhasil')
         print('=========================')
-        
-        saldo1= saldo - tarik
- 
-        if saldo1 >=0:
-        	saldo = saldo1
-        	print('=====================================')
-        	print('saldo anda saat ini: Rp',saldo)
-        	print('=====================================')
-        
+        cek_saldo()
+
 def tambah_saldo():
     global saldo
-    tambah=float(input('tambah saldo: Rp'))
-    saldonew= saldo + tambah
-    
-    if tambah >0:
-        saldo= saldonew
+    tambah = float(input('tambah saldo: Rp'))
+    if tambah > 0:
+        saldo += tambah
         print('===========================')
         print('saldo berhasil ditambahkan')
         print('===========================')
     cek_saldo()
-        
+
 while True:
     print('Menu')
-    print('1.Cek saldo')
-    print('2.Transfer')
-    print('3.Tarik tunai')
-    print('4.Tambah saldo')
-    print('5.Keluar')
-    pilihan=input('pilih menu:')
-    
+    print('1. Cek saldo')
+    print('2. Transfer')
+    print('3. Tarik tunai')
+    print('4. Tambah saldo')
+    print('5. Keluar')
+    pilihan = input('pilih menu:')
+
     if pilihan == '1':
         cek_saldo()
     elif pilihan == '2':
@@ -77,5 +69,6 @@ while True:
         tambah_saldo()
     elif pilihan == '5':
         print('terima kasih telah menggunakan layanan kami')
+        break  # Keluar dari loop jika pilihan 5 dipilih
     else:
         print('pilihan tidak valid')
